f30380af400f870622bc88305e5cf3e3bea195ee
Andrew Levin <andrew.levin77@hotmail.com>
2016-07-03 10:12:15 +0300

    Migrate to net core release.

--- downloadDotNet.ps1
+++ downloadDotNet.ps1
@@ -1,5 +1,5 @@
 function Download-DotNet {
-    $DOTNET_SDK_URL = 'https://dotnetcli.blob.core.windows.net/dotnet/beta/Binaries/Latest/dotnet-dev-win-x64.latest.zip'
+    $DOTNET_SDK_URL = 'https://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/packages/DotNetCore.1.0.0-SDK.Preview2-x64.exe'
     Invoke-WebRequest -Uri $DOTNET_SDK_URL -OutFile dotnet.zip
     Add-Type -assembly 'system.io.compression.filesystem'
     if (Test-Path '.\dist-dotnet'){
