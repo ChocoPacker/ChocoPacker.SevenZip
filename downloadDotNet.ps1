d8dd0faacf632e89315e718f56383f2bb023b15c
Andrew Levin <andrew.levin77@hotmail.com>
2016-07-04 14:17:22 +0300

    Fix dotnet URL

--- downloadDotNet.ps1
+++ downloadDotNet.ps1
@@ -1,5 +1,5 @@
 function Download-DotNet {
-    $DOTNET_SDK_URL = 'https://download.microsoft.com/download/A/3/8/A38489F3-9777-41DD-83F8-2CBDFAB2520C/packages/DotNetCore.1.0.0-SDK.Preview2-x64.exe'
+    $DOTNET_SDK_URL = 'https://download.microsoft.com/download/1/5/2/1523EBE1-3764-4328-8961-D1BD8ECA9295/dotnet-dev-win-x64.1.0.0-preview2-003121.zip'
     Invoke-WebRequest -Uri $DOTNET_SDK_URL -OutFile dotnet.zip
     Add-Type -assembly 'system.io.compression.filesystem'
     if (Test-Path '.\dist-dotnet'){
